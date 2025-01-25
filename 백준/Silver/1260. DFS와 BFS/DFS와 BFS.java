2c14d1bc1024ef2ae9cf61789d29aad92aa7485c
jhyang00815 <jhyang00815@gmail.com>
2025-01-25 13:02:45 +0900

    Update DFS와 BFS.java

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,5 +1,3 @@
-//TIP 코드를 <b>실행</b>하려면 <shortcut actionId="Run"/>을(를) 누르거나
-// 에디터 여백에 있는 <icon src="AllIcons.Actions.Execute"/> 아이콘을 클릭하세요.
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -7,27 +5,27 @@ import java.util.*;
 public class Main {
 
     public static void DFS(int[][] graph,int n, boolean[] visited){
-        visited[n]=true;
-        System.out.print(n+" ");
+        visited[n]=true; //방문처리
+        System.out.print(n+" "); //방문 노드 출력
         //System.out.print(graph.length);
-        for(int i=1;i<graph.length;i++){ //n이 계속 바뀌므로 i<n+1이 아니라 i<graph.length 로
-            if(graph[n][i]==1 && !visited[i]){
-                // DFS(graph,i,visited); //i를 그대로 넣어버리면 visited[i-1]에서 문제가 생긴다.
-                DFS(graph,i,visited);
+        for(int i=1;i<graph.length;i++){      //1부터 n까지 n개의 노드 전부 확인
+                                              //(n이 계속 바뀌므로 i<n+1이 아니라 i<graph.length 로)
+            if(graph[n][i]==1 && !visited[i]){ //노드 n과 연결되어있고 방문하지 않았다면                
+                DFS(graph,i,visited);          //DFS 재귀 수행
             }
         }
     }
     public static void BFS(int[][] graph,int n, boolean[] visited){
         Queue<Integer> que = new LinkedList<>();
-        que.add(n);
-        visited[n] = true;
+        que.add(n);   //큐에 노드 넣기
+        visited[n] = true;  //노드 방문처리
         while(!que.isEmpty()){
-            n=que.poll();
-            System.out.print(n+" ");
+            n=que.poll();    //방문한 노드 큐에서 제거
+            System.out.print(n+" ");    //방문 노드 출력
             for(int i=1;i<graph.length;i++){
-                if(graph[n][i]==1 && !visited[i]){
-                    que.add(i);
-                    visited[i] = true;
+                if(graph[n][i]==1 && !visited[i]){    //노드 n과 연결되어있고 아직 방문 안했다면
+                    que.add(i);                       //큐에 넣고 방문처리
+                    visited[i] = true;                
                 }
             }
         }
@@ -42,10 +40,10 @@ public class Main {
         int m = Integer.parseInt(num[1]);
         int v = Integer.parseInt(num[2]);
 
-        int[][] graph= new int[n+1][n+1];
-        boolean[] visited= new boolean[n+1];
+        int[][] graph= new int[n+1][n+1]; //n+1 X n+1 행렬로 생성(인덱스 n으로 접근)
+        boolean[] visited= new boolean[n+1]; 
         for(int i=0;i<n+1;i++) {
-            visited[i]=false;
+            visited[i]=false; 
         }
         //graph initialize
         for(int i=0;i<m;i++){
@@ -64,4 +62,3 @@ public class Main {
         BFS(graph,v,visited);
     }
 }
-
